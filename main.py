860a5abb6ef0ae68eeca22180b7f457d52064235
Kyle Benson <kyleabenson@gmail.com>
2025-06-02 21:52:19 +0000

    added internal tracer

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import fastapi
 import time
 import random 
 import logging
-
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+# Acquire a tracer
+tracer = trace.get_tracer("diceroller.tracer")
+@tracer.start_as_current_span("roll_d6")
 def roll_d6():
   value = random.randint(1, 6)
   if value == 6:
@@ -14,8 +20,6 @@ def roll_d6():
   return value
 
 app = fastapi.FastAPI()
-
-logging.basicConfig(level=logging.INFO)
 
 @app.get("/")
 async def foobar():
